0eb26ebfe82ada79bd1b4b9a180deec138aa7c46
Drforkert <walteregon55@gmail.com>
2021-04-06 19:09:46 +0100

    swiper loop

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,9 @@
 const swiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
+    autoplay: {
+      delay: 5000
+    },
   
     pagination: {
       el: '.swiper-pagination',
